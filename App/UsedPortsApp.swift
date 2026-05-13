10b11fe07207043dfd78ba29713e50ea51f3857a
Injae Ryou <injaeryou@gmail.com>
2026-05-13 19:22:47 +0900

    scaffold: xcodegen project with UsedPorts app, uph helper, test targets

---
+++ App/UsedPortsApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct UsedPortsApp: App {
+    var body: some Scene {
+        WindowGroup("UsedPorts") {
+            Text("Hello")
+                .frame(minWidth: 800, minHeight: 500)
+        }
+    }
+}
